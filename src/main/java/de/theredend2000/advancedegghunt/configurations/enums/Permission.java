9db1ff4873921245c7d0354470064275e5d85d51
GrafterCrafter <>
2024-03-05 20:26:10 +0000

    Permission enum (WIP)

--- src/main/java/de/theredend2000/advancedegghunt/configurations/enums/Permission.java
+++ src/main/java/de/theredend2000/advancedegghunt/configurations/enums/Permission.java
@@ -1,16 +1,22 @@
 package de.theredend2000.advancedegghunt.configurations.enums;
 
 public enum Permission {
-    placeEggs,
-    list,
-    show,
-    reload,
-    help,
-    settings,
-    collection,
-    progress,
-    commands,
-    leaderboard,
-    hint,
-    reset,
+    BreakEggPermission,
+    PlaceEggPermission,
+    IgnoreCooldownPermission,
+    ChangeCollectionsPermission;
+    public enum AdvancedEggHuntCommandPermissionCommand {
+        placeEggs,
+        list,
+        show,
+        reload,
+        help,
+        settings,
+        collection,
+        progress,
+        commands,
+        leaderboard,
+        hint,
+        reset;
+    }
 }
