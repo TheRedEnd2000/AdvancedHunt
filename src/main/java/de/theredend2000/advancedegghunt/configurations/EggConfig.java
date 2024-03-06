3d747ea403f2465ccf81d5a9c2f25708145289b8
GrafterCrafter <>
2024-03-06 09:09:56 +0000

    YML Configuration Files (WIP)

--- src/main/java/de/theredend2000/advancedegghunt/configurations/EggConfig.java
+++ src/main/java/de/theredend2000/advancedegghunt/configurations/EggConfig.java
@@ -2,8 +2,10 @@ package de.theredend2000.advancedegghunt.configurations;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.text.MessageFormat;
+
 public class EggConfig extends Configuration {
     public EggConfig(JavaPlugin plugin, String configName) {
-        super(plugin, configName);
+        super(plugin, MessageFormat.format("/eggs/{0}", configName));
     }
 }
