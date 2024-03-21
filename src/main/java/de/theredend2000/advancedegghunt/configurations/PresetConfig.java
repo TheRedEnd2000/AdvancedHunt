0a1a849062235444dc646a96aefabd0a7291429f
GrafterCrafter <>
2024-03-21 11:12:33 +0000

    Create PresetConfig and prep EggConfig for later use

--- src/main/java/de/theredend2000/advancedegghunt/configurations/PresetConfig.java
+++ src/main/java/de/theredend2000/advancedegghunt/configurations/PresetConfig.java
@@ -5,11 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.text.MessageFormat;
 import java.util.TreeMap;
 
-public class EggConfig extends Configuration {
+public class PresetConfig extends Configuration {
     private static TreeMap<Double, ConfigUpgrader> upgraders = new TreeMap<>();
 
-    public EggConfig(JavaPlugin plugin, String configName) {
-        super(plugin, MessageFormat.format("eggs/{0}.yml", configName), false);
+    public PresetConfig(JavaPlugin plugin, String configName) {
+        super(plugin, MessageFormat.format("presets/{0}.yml", configName), false);
     }
 
     @Override
