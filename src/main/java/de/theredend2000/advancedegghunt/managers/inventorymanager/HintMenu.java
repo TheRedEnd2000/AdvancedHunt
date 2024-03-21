7795d9013f05586a4b8fb2a400dcab80ca5e3a79
GrafterCrafter <>
2024-03-21 11:18:30 +0000

    Convert GUI to use new abstract classes (WIP)

---
+++ src/main/java/de/theredend2000/advancedegghunt/managers/inventorymanager/HintMenu.java
@@ -0,0 +1,136 @@
+package de.theredend2000.advancedegghunt.managers.inventorymanager;
+
+import com.cryptomorin.xseries.XMaterial;
+import de.theredend2000.advancedegghunt.Main;
+import de.theredend2000.advancedegghunt.managers.SoundManager;
+import de.theredend2000.advancedegghunt.managers.inventorymanager.common.InventoryMenu;
+import de.theredend2000.advancedegghunt.util.ConfigLocationUtil;
+import de.theredend2000.advancedegghunt.util.ItemBuilder;
+import de.theredend2000.advancedegghunt.util.PlayerMenuUtility;
+import de.theredend2000.advancedegghunt.util.messages.MessageKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Random;
+
+public class HintMenu extends InventoryMenu {
+    private boolean active;
+    private int currentSlot;
+    private int currentCount;
+    private boolean clickedRight;
+    private int lastClicked;
+    private Random random;
+
+    public HintMenu(PlayerMenuUtility playerMenuUtility) {
+        super(playerMenuUtility, "Eggs Hint", (short) 54);
+    }
+
+    public void open(boolean active) {
+        if (playerMenuUtility.getOwner() == null) {
+            return;
+        }
+
+        this.active = active;
+        this.clickedRight = true;
+        this.currentCount = 0;
+        this.lastClicked = -1;
+        random = new Random();
+
+        playerMenuUtility.getOwner().openInventory(getInventory());
+
+        for (int i = 0; i < getInventory().getSize(); i++){getInventory().setItem(i, new ItemBuilder(XMaterial.RED_STAINED_GLASS_PANE).setDisplayname("§c").build());}
+        startAnimating();
+        Main.getInstance().getCooldownManager().setCooldown(playerMenuUtility.getOwner());
+    }
+
+    private void startAnimating(){
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(active){
+                    if(!clickedRight) {
+                        fail(playerMenuUtility.getOwner());
+                        playerMenuUtility.getOwner().sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.CLICKED_SAME));
+                        return;
+                    }
+
+                    getInventory().setItem(currentSlot, new ItemBuilder(XMaterial.RED_STAINED_GLASS_PANE).setDisplayname("§c").build());
+                    currentSlot = getRandomSlot();
+                    getInventory().setItem(currentSlot, new ItemBuilder(XMaterial.LIME_STAINED_GLASS_PANE).setDisplayname("§aConfirm").setLore("§6" + (currentCount + 1) + "§7/§6" + Main.getInstance().getPluginConfig().getHintCount()).build());
+                    clickedRight = false;
+                }else{
+                    cancel();
+                }
+            }
+        }.runTaskTimer(Main.getInstance(), 40, Main.getInstance().getPluginConfig().getHintUpdateTime());
+    }
+
+    private int getRandomSlot(){
+        int nextNum;
+        do {
+            nextNum = random.nextInt(getInventory().getSize());
+        } while (nextNum == lastClicked);
+        return nextNum;
+    }
+
+    public void fail(Player player){
+        player.closeInventory();
+        player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playErrorSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+        active = false;
+    }
+
+    public String getReward(Player player){
+        Main plugin = Main.getInstance();
+        for(String collection : plugin.getEggDataManager().savedEggCollections()) {
+            if (!Main.getInstance().getEggManager().containsPlayer(player.getName())) {
+                continue;
+            }
+            if (Main.getInstance().getEggManager().checkFoundAll(player, collection)) continue;
+            int number = Main.getInstance().getEggManager().getRandomNotFoundEgg(player, collection);
+            ConfigLocationUtil location = new ConfigLocationUtil(plugin, "PlacedEggs." + number + ".");
+            if (location.loadLocation(collection) != null) {
+                int random = new Random().nextInt(2);
+                return Main.getInstance().getMessageManager().getMessage(MessageKey.EGG_HINT).replaceAll("%X%", random == 1 ? String.valueOf(location.loadLocation(collection).getBlockX()) : "§k1").replaceAll("%Y%", String.valueOf(location.loadLocation(collection).getBlockY())).replaceAll("%Z%", random == 0 ? String.valueOf(location.loadLocation(collection).getBlockZ()) : "§k1");
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String getMenuName() {
+        return null;
+    }
+
+    @Override
+    public int getSlots() {
+        return 0;
+    }
+
+    @Override
+    public void handleMenu(InventoryClickEvent event) {
+        SoundManager soundManager = Main.getInstance().getSoundManager();
+
+        if (currentSlot == lastClicked) {
+            fail(playerMenuUtility.getOwner());
+            playerMenuUtility.getOwner().sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.CLICKED_SAME));
+            return;
+        }
+        if (event.getSlot() == currentSlot) {
+            currentCount++;
+            playerMenuUtility.getOwner().playSound(playerMenuUtility.getOwner().getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
+            if (currentCount == Main.getInstance().getPluginConfig().getHintCount()) {
+                playerMenuUtility.getOwner().closeInventory();
+                active = false;
+                playerMenuUtility.getOwner().playSound(playerMenuUtility.getOwner().getLocation(), soundManager.playAllEggsFound(), soundManager.getSoundVolume(), 1);
+                playerMenuUtility.getOwner().sendMessage(getReward(playerMenuUtility.getOwner()));
+            }
+            clickedRight = true;
+            lastClicked = currentSlot;
+        } else {
+            fail(playerMenuUtility.getOwner());
+            playerMenuUtility.getOwner().sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.CLICKED_SAME));
+        }
+    }
+}
+
---
+++ src/main/java/de/theredend2000/advancedegghunt/managers/inventorymanager/PresetsMenu.java
@@ -0,0 +1,152 @@
+package de.theredend2000.advancedegghunt.managers.inventorymanager;
+
+import com.cryptomorin.xseries.XMaterial;
+import de.theredend2000.advancedegghunt.Main;
+import de.theredend2000.advancedegghunt.managers.PresetDataManager;
+import de.theredend2000.advancedegghunt.managers.inventorymanager.common.PaginatedInventoryMenu;
+import de.theredend2000.advancedegghunt.util.ItemBuilder;
+import de.theredend2000.advancedegghunt.util.PlayerMenuUtility;
+import de.theredend2000.advancedegghunt.util.messages.MessageKey;
+import de.theredend2000.advancedegghunt.util.messages.MessageManager;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+import java.util.ArrayList;
+
+public class PresetsMenu extends PaginatedInventoryMenu {
+    private MessageManager messageManager;
+    private Main plugin;
+    private String id;
+    private String collection;
+    public PresetsMenu(PlayerMenuUtility playerMenuUtility) {
+        super(playerMenuUtility, "Presets", (short) 54);
+        this.plugin = Main.getInstance();
+        this.messageManager = this.plugin.getMessageManager();
+    }
+
+    public void open(String id, String collection) {
+        this.collection = collection;
+        this.id = id;
+
+        super.addMenuBorder();
+        addMenuBorder();
+        setMenuItems();
+
+        playerMenuUtility.getOwner().openInventory(getInventory());
+    }
+
+    public void addMenuBorder() {
+        inventoryContent[48] = new ItemBuilder(XMaterial.PLAYER_HEAD).setSkullOwner(Main.getTexture("ZDU5YmUxNTU3MjAxYzdmZjFhMGIzNjk2ZDE5ZWFiNDEwNDg4MGQ2YTljZGI0ZDVmYTIxYjZkYWE5ZGIyZDEifX19")).setLore("§6Page: §7(§b" + (page + 1) + "§7/§b" + getMaxPages() + "§7)", "", "§eClick to scroll.").setDisplayname("§2Left").build();
+
+        inventoryContent[50] = new ItemBuilder(XMaterial.PLAYER_HEAD).setSkullOwner(Main.getTexture("NDJiMGMwN2ZhMGU4OTIzN2Q2NzllMTMxMTZiNWFhNzVhZWJiMzRlOWM5NjhjNmJhZGIyNTFlMTI3YmRkNWIxIn19fQ==")).setLore("§6Page: §7(§b" + (page + 1) + "§7/§b" + getMaxPages() + "§7)", "", "§eClick to scroll.").setDisplayname("§2Right").build();
+        inventoryContent[49] = new ItemBuilder(XMaterial.BARRIER).setDisplayname("§cClose").build();
+        inventoryContent[45] = new ItemBuilder(XMaterial.PLAYER_HEAD).setSkullOwner(Main.getTexture("ODFjOTZhNWMzZDEzYzMxOTkxODNlMWJjN2YwODZmNTRjYTJhNjUyNzEyNjMwM2FjOGUyNWQ2M2UxNmI2NGNjZiJ9fX0=")).setDisplayname("§eBack").build();
+    }
+
+    public void setMenuItems() {
+        PresetDataManager presetDataManager = Main.getInstance().getPresetDataManager();
+        ArrayList<String> keys = new ArrayList<>();
+        if(presetDataManager.savedPresets().size() >= 1){
+            keys.addAll(presetDataManager.savedPresets());
+        }else
+            getInventory().setItem(22, new ItemBuilder(XMaterial.RED_STAINED_GLASS).setDisplayname("§4§lNo Presets").setLore("§7Create new one to select them.").build());
+        if (keys == null || keys.isEmpty()) {
+            getInventory().setItem(22, new ItemBuilder(XMaterial.RED_STAINED_GLASS).setDisplayname("§4§lNo Presets").setLore("§7Create new one to select them.").build());
+            return;
+        }
+        for(int i = 0; i < maxItemsPerPage; i++) {
+            index = maxItemsPerPage * page + i;
+            if(index >= keys.size()) break;
+            if (keys.get(index) != null){
+                String defaultPreset = plugin.getPluginConfig().getDefaultLoadingPreset();
+                getInventory().addItem(new ItemBuilder(XMaterial.PAPER).setDisplayname("§b§l" + keys.get(index)).setDefaultLore(presetDataManager.getAllCommandsAsLore(keys.get(index), keys.get(index).equals(defaultPreset))).setLocalizedName(keys.get(index)).build());
+            }
+        }
+    }
+
+    public int getMaxPages(){
+        PresetDataManager presetDataManager = Main.getInstance().getPresetDataManager();
+        ArrayList<String> keys = new ArrayList<>(presetDataManager.savedPresets());
+        if(keys.isEmpty()) return 1;
+        return (int) Math.ceil((double) keys.size() / maxItemsPerPage);
+    }
+
+    @Override
+    public String getMenuName() {
+        return null;
+    }
+
+    @Override
+    public int getSlots() {
+        return 0;
+    }
+
+    @Override
+    public void handleMenu(InventoryClickEvent event) {
+        Player player = (Player) event.getWhoClicked();
+        PresetDataManager presetDataManager = Main.getInstance().getPresetDataManager();
+        if(event.getCurrentItem() == null) return;
+
+        for(String presetName : presetDataManager.savedPresets()){
+            if(event.getCurrentItem() == null || event.getCurrentItem().getItemMeta() == null) continue;
+            if (!ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).equals(presetName)) {
+                return;
+            }
+            switch (event.getAction()) {
+                case PICKUP_ALL:
+                    player.sendMessage(messageManager.getMessage(MessageKey.PRESET_LOADED).replaceAll("%PRESET%", presetName));
+                    presetDataManager.loadPresetIntoEggCommands(presetName, collection, id);
+                    plugin.getEggRewardsInventory().open(super.playerMenuUtility.getOwner(), id, collection);
+                    break;
+                case PICKUP_HALF:
+                    if (!plugin.getPluginConfig().getDefaultLoadingPreset().equals(presetName)) {
+                        presetDataManager.deletePreset(presetName);
+                        player.sendMessage(messageManager.getMessage(MessageKey.PRESET_DELETE).replaceAll("%PRESET%", presetName));
+                    } else
+                        player.sendMessage(messageManager.getMessage(MessageKey.PRESET_NOT_DELETE_DEFAULT).replaceAll("%PRESET%", presetName));
+                    open(id, collection);
+                    break;
+                case CLONE_STACK:
+                    plugin.getPluginConfig().setDefaultLoadingPreset(presetName);
+                    player.sendMessage(messageManager.getMessage(MessageKey.PRESET_DEFAULT).replaceAll("%PRESET%", presetName));
+                    open(id, collection);
+                    break;
+            }
+            player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+        }
+
+        XMaterial material = XMaterial.matchXMaterial(event.getCurrentItem());
+        switch (material) {
+            case BARRIER:
+                player.closeInventory();
+                player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                break;
+            case PLAYER_HEAD:
+                if (ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).equalsIgnoreCase("Left")) {
+                    if (page == 0) {
+                        player.sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.FIRST_PAGE));
+                        player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventoryFailedSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                    } else {
+                        page = page - 1;
+                        open(id, collection);
+                        player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                    }
+                } else if (ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).equalsIgnoreCase("Right")) {
+                    if (!((index + 1) >= presetDataManager.savedPresets().size())) {
+                        page = page + 1;
+                        open(id, collection);
+                        player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                    } else {
+                        player.sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.LAST_PAGE));
+                        player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventoryFailedSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                    }
+                } else if (ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).equalsIgnoreCase("Back")) {
+                    player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                    plugin.getEggRewardsInventory().open(super.playerMenuUtility.getOwner(), id, collection);
+                }
+                break;
+        }
+    }
+}
+
