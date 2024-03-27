a2e16ec867b370e49f8b955fce694b974de4fe45
TheRedEnd2000 <fkircher2007@gmail.com>
2024-03-27 17:31:37 +0100

    default changes

--- src/main/java/de/theredend2000/advancedegghunt/managers/inventorymanager/eggrewards/global/GlobalPresetDataManager.java
+++ src/main/java/de/theredend2000/advancedegghunt/managers/inventorymanager/eggrewards/global/GlobalPresetDataManager.java
@@ -146,12 +146,10 @@ public class GlobalPresetDataManager {
 
     public void addDefaultRewardCommands(String preset) {
         FileConfiguration config = this.getPresets(preset);
-        config.set("Commands.0.command", "tellraw %PLAYER% \"%PREFIX%&aYou found an egg. &7(&e%EGGS_FOUND%&7/&e%EGGS_MAX%&7)\"");
+        config.set("Commands.0.command", "minecraft:give %PLAYER% diamond");
         config.set("Commands.0.enabled", true);
-        config.set("Commands.1.command", "minecraft:give %PLAYER% diamond");
+        config.set("Commands.1.command", "tellraw %PLAYER% \"%PREFIX%&6You found all eggs!\"");
         config.set("Commands.1.enabled", true);
-        config.set("Commands.2.command", "tellraw %PLAYER% \"%PREFIX%&6You found all eggs!\"");
-        config.set("Commands.2.enabled", true);
         this.savePreset(preset, config);
     }
 
