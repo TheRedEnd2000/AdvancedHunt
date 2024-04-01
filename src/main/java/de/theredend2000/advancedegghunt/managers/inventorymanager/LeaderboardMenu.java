cb40083b7f40d331281df04823ef3c38258709a7
GrafterCrafter <>
2024-04-01 13:46:08 +0100

    Fix leaderboard bug

--- src/main/java/de/theredend2000/advancedegghunt/managers/inventorymanager/LeaderboardMenu.java
+++ src/main/java/de/theredend2000/advancedegghunt/managers/inventorymanager/LeaderboardMenu.java
@@ -27,10 +27,16 @@ public class LeaderboardMenu extends PaginatedInventoryMenu {
     }
 
     public void open() {
+        page = 0;
         getInventory().setContents(inventoryContent);
         setMenuItems();
 
         playerMenuUtility.getOwner().openInventory(getInventory());
+    }
+
+    public void reopen() {
+        getInventory().setContents(inventoryContent);
+        setMenuItems();
     }
 
     public void addMenuBorderButtons() {
@@ -94,6 +100,29 @@ public class LeaderboardMenu extends PaginatedInventoryMenu {
         }
 
         leaderList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+        int maxEggs = Main.getInstance().getEggManager().getMaxEggs(collection);
+
+        if (sortTypes.equals(LeaderboardSortTypes.YOU)) {
+            index = 0;
+            numberOfPlayers = 1;
+
+            for(int i = 0; i < leaderboard.size(); i++) {
+                String playerName = leaderList.get(i).getKey();
+                if (!playerName.equals(playerMenuUtility.getOwner().getName())) {
+                    continue;
+                }
+
+                int count = leaderList.get(i).getValue();
+                getInventory().addItem(new ItemBuilder(XMaterial.PLAYER_HEAD)
+                        .setOwner(playerName)
+                        .setDisplayname("§6§l" + (i + 1) + "§6th §2§n" + playerName + (playerName.equals(playerMenuUtility.getOwner().getName()) ? "§r §a§lYOU" : ""))
+                        .setLore("", "§7Eggs Found: §3" + count, "§7Eggs Remaining: §3" + (maxEggs - count), "§7Max Eggs: §3" + maxEggs, "", 9 >= i ? "§eTHIS PLAYER IS IN THE TOP 10!" : "§c" + (i - 9) + " place behind 10th place").build());
+
+                return;
+            }
+            getInventory().setItem(22, new ItemBuilder(XMaterial.RED_STAINED_GLASS).setDisplayname("§4§lNo Player").setLore("§7There are no players in the leaderboard.").build());
+            return;
+        }
 
         for(int i = 0; i < getMaxItemsPerPage(); i++) {
             index = (getMaxItemsPerPage() * page) + i;
@@ -102,7 +131,7 @@ public class LeaderboardMenu extends PaginatedInventoryMenu {
 
             String playerName = leaderList.get(index).getKey();
             int count = leaderList.get(index).getValue();
-            int maxEggs = Main.getInstance().getEggManager().getMaxEggs(collection);
+
             switch (sortTypes) {
                 case ALL:
                     getInventory().setItem(slotIndex, new ItemBuilder(XMaterial.PLAYER_HEAD).setOwner(playerName)
@@ -110,6 +139,7 @@ public class LeaderboardMenu extends PaginatedInventoryMenu {
                             .setLore("", "§7Eggs Found: §3" + count, "§7Eggs Remaining: §3" + (maxEggs - count), "§7Max Eggs: §3" + maxEggs, "", 9 >= i ? "§eTHIS PLAYER IS IN THE TOP 10!" : "§c" + (i - 9) + " place behind 10th place").build());
                     break;
                 case TOP3:
+                    numberOfPlayers = 3;
                     if (i < 3) {
                         getInventory().setItem(slotIndex, new ItemBuilder(XMaterial.PLAYER_HEAD).setOwner(playerName)
                                 .setDisplayname("§6§l" + (index + 1) + "§6th §2§n" + playerName + (playerName.equals(playerMenuUtility.getOwner().getName()) ? "§r §a§lYOU" : ""))
@@ -117,18 +147,12 @@ public class LeaderboardMenu extends PaginatedInventoryMenu {
                     }
                     break;
                 case TOP10:
+                    numberOfPlayers = 10;
                     if (i < 10) {
                         getInventory().setItem(slotIndex, new ItemBuilder(XMaterial.PLAYER_HEAD).setOwner(playerName)
                                 .setDisplayname("§6§l" + (index + 1) + "§6th §2§n" + playerName + (playerName.equals(playerMenuUtility.getOwner().getName()) ? "§r §a§lYOU" : ""))
                                 .setLore("", "§7Eggs Found: §3" + count, "§7Eggs Remaining: §3" + (maxEggs - count), "§7Max Eggs: §3" + maxEggs, "", "§eTHIS PLAYER IS IN THE TOP 10!").build());
                     }
-                    break;
-                case YOU:
-                    if (playerName.equals(playerMenuUtility.getOwner().getName()))
-                        getInventory().addItem(new ItemBuilder(XMaterial.PLAYER_HEAD)
-                                .setOwner(playerName)
-                                .setDisplayname("§6§l" + (index + 1) + "§6th §2§n" + playerName + (playerName.equals(playerMenuUtility.getOwner().getName()) ? "§r §a§lYOU" : ""))
-                                .setLore("", "§7Eggs Found: §3" + count, "§7Eggs Remaining: §3" + (maxEggs - count), "§7Max Eggs: §3" + maxEggs, "", 9 >= i ? "§eTHIS PLAYER IS IN THE TOP 10!" : "§c" + (i - 9) + " place behind 10th place").build());
                     break;
             }
         }
@@ -185,13 +209,13 @@ public class LeaderboardMenu extends PaginatedInventoryMenu {
                         player.playSound(player.getLocation(), soundManager.playInventoryFailedSound(), soundManager.getSoundVolume(), 1);
                     } else {
                         page = page - 1;
-                        open();
+                        reopen();
                         player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                     }
                 } else if (ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName()).equalsIgnoreCase("Right")) {
                     if (!((index + 1) >= numberOfPlayers)) {
                         page = page + 1;
-                        open();
+                        reopen();
                         player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                     } else {
                         player.sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.LAST_PAGE));
