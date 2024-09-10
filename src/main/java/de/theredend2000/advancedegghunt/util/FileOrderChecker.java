9e4d32449be4cec58a1ede6cb4c3a7c958de2ae2
TheRedEnd2000 <fkircher2007@gmail.com>
2024-09-10 19:42:12 +0200

    fixed isFileNameFirst not supporting versiosn longer that 2 like 2.11.5

--- src/main/java/de/theredend2000/advancedegghunt/util/FileOrderChecker.java
+++ src/main/java/de/theredend2000/advancedegghunt/util/FileOrderChecker.java
@@ -1,5 +1,7 @@
 package de.theredend2000.advancedegghunt.util;
 
+import org.bukkit.Bukkit;
+
 import java.io.File;
 
 public class FileOrderChecker {
@@ -39,6 +41,39 @@ public class FileOrderChecker {
      * @return true if filename1 comes before filename2 alphabetically, false otherwise
      */
     public static boolean isFileNameFirst(String filename1, String filename2) {
-        return filename1.compareToIgnoreCase(filename2) < 0;
+        String version1 = extractVersion(filename1);
+        String version2 = extractVersion(filename2);
+
+        return compareVersions(version1, version2) > 0;
     }
+
+    private static String extractVersion(String filename) {
+        int dashIndex = filename.lastIndexOf('-');
+        int dotJarIndex = filename.lastIndexOf(".jar");
+
+        if (dashIndex != -1 && dotJarIndex != -1 && dashIndex < dotJarIndex) {
+            return filename.substring(dashIndex + 1, dotJarIndex);
+        }
+
+        return "";
+    }
+
+    private static int compareVersions(String version1, String version2) {
+        String[] parts1 = version1.split("\\.");
+        String[] parts2 = version2.split("\\.");
+
+        int length = Math.max(parts1.length, parts2.length);
+
+        for (int i = 0; i < length; i++) {
+            int v1 = i < parts1.length ? Integer.parseInt(parts1[i]) : 0;
+            int v2 = i < parts2.length ? Integer.parseInt(parts2[i]) : 0;
+
+            if (v1 != v2) {
+                return Integer.compare(v1, v2);
+            }
+        }
+
+        return 0;
+    }
+
 }
