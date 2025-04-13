bde5c9ad08f193e0ad37a08cd3e26d034aa8411b
TheRedEnd2000 <fkircher2007@gmail.com>
2025-04-13 22:11:50 +0200

    made settings menu scrollable

--- src/main/java/de/theredend2000/advancedhunt/managers/inventorymanager/SettingsMenu.java
+++ src/main/java/de/theredend2000/advancedhunt/managers/inventorymanager/SettingsMenu.java
@@ -3,7 +3,8 @@ package de.theredend2000.advancedhunt.managers.inventorymanager;
 import com.cryptomorin.xseries.XMaterial;
 import de.theredend2000.advancedhunt.Main;
 import de.theredend2000.advancedhunt.managers.SoundManager;
-import de.theredend2000.advancedhunt.managers.inventorymanager.common.InventoryMenu;
+import de.theredend2000.advancedhunt.managers.inventorymanager.common.IInventoryMenuOpen;
+import de.theredend2000.advancedhunt.managers.inventorymanager.common.PaginatedInventoryMenu;
 import de.theredend2000.advancedhunt.util.ItemBuilder;
 import de.theredend2000.advancedhunt.util.ItemHelper;
 import de.theredend2000.advancedhunt.util.PlayerMenuUtility;
@@ -14,9 +15,15 @@ import org.apache.commons.lang.StringUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.ItemStack;
 
-public class SettingsMenu extends InventoryMenu {
+import java.util.ArrayList;
+
+public class SettingsMenu extends PaginatedInventoryMenu implements IInventoryMenuOpen {
+
     private MessageManager messageManager;
+    private ArrayList<ItemStack> keys = new ArrayList<>();
 
     public SettingsMenu(PlayerMenuUtility playerMenuUtility) {
         super(playerMenuUtility, "Advanced " + StringUtils.capitalize(Main.getInstance().getPluginConfig().getPluginNameSingular()) + " Settings", (short) 54, XMaterial.RED_STAINED_GLASS_PANE);
@@ -24,23 +31,17 @@ public class SettingsMenu extends InventoryMenu {
     }
 
     public void open() {
-        menuContent();
+        fillMenuItems();
+        super.addMenuBorder();
         addMenuBorderButtons();
-        menuContent();
+        setMenuItems();
 
         playerMenuUtility.getOwner().openInventory(getInventory());
     }
 
-    private void addMenuBorderButtons() {
-        inventoryContent[49] = new ItemBuilder(XMaterial.BARRIER)
-                .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.CLOSE_BUTTON))
-                .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.CLOSE_BUTTON))
-                .setCustomId("settings.close")
-                .build();
-    }
-
-    private void menuContent() {
-        getInventory().setItem(10, new ItemBuilder(XMaterial.CLOCK)
+    private void fillMenuItems(){
+        keys.clear();
+        keys.add(new ItemBuilder(XMaterial.CLOCK)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_UPDATER))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_UPDATER,
                         "%STATUS%", Main.getInstance().getPluginConfig().getUpdater() ? "§a§l✔ Enabled" : "§c§l❌ Disabled"))
@@ -48,14 +49,14 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(Main.getInstance().getPluginConfig().getUpdater())
                 .build());
 
-        getInventory().setItem(11, new ItemBuilder(XMaterial.COMMAND_BLOCK)
+        keys.add(new ItemBuilder(XMaterial.COMMAND_BLOCK)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_COMMAND_FEEDBACK))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_COMMAND_FEEDBACK,
                         "%STATUS%", "§c§l❌ Discontinued"))
                 .setCustomId("settings.commandfeedback")
                 .build());
 
-        getInventory().setItem(12, new ItemBuilder(XMaterial.NOTE_BLOCK)
+        keys.add(new ItemBuilder(XMaterial.NOTE_BLOCK)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_SOUND_VOLUME))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_SOUND_VOLUME,
                         "%VOLUME%", "§6" + Main.getInstance().getPluginConfig().getSoundVolume()))
@@ -63,7 +64,7 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(true)
                 .build());
 
-        getInventory().setItem(13, new ItemBuilder(XMaterial.COMPASS)
+        keys.add(new ItemBuilder(XMaterial.COMPASS)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_SHOW_COORDINATES))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_SHOW_COORDINATES,
                         "%STATUS%", Main.getInstance().getPluginConfig().getShowCoordinatesWhenEggFoundInProgressInventory() ? "§a§l✔ Enabled" : "§c§l❌ Disabled"))
@@ -71,7 +72,7 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(Main.getInstance().getPluginConfig().getShowCoordinatesWhenEggFoundInProgressInventory())
                 .build());
 
-        getInventory().setItem(14, new ItemBuilder(XMaterial.ARMOR_STAND)
+        keys.add(new ItemBuilder(XMaterial.ARMOR_STAND)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_ARMORSTAND_GLOW))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_ARMORSTAND_GLOW,
                         "%GLOW_DURATION%", "§6" + Main.getInstance().getPluginConfig().getArmorstandGlow()))
@@ -79,7 +80,7 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(true)
                 .build());
 
-        getInventory().setItem(15, new ItemBuilder(XMaterial.OAK_SIGN)
+        keys.add(new ItemBuilder(XMaterial.OAK_SIGN)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_EGG_NEARBY_RADIUS))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_EGG_NEARBY_RADIUS,
                         "%RADIUS%", "§6" + Main.getInstance().getPluginConfig().getShowEggsNearbyMessageRadius()))
@@ -87,7 +88,7 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(true)
                 .build());
 
-        getInventory().setItem(16, new ItemBuilder(XMaterial.NAME_TAG)
+        keys.add(new ItemBuilder(XMaterial.NAME_TAG)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_PLUGIN_PREFIX))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_PLUGIN_PREFIX,
                         "%STATUS%", Main.getInstance().getPluginConfig().getPluginPrefixEnabled() ? "§a§l✔ Enabled" : "§c§l❌ Disabled"))
@@ -95,7 +96,7 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(Main.getInstance().getPluginConfig().getPluginPrefixEnabled())
                 .build());
 
-        getInventory().setItem(19, new ItemBuilder(XMaterial.FIREWORK_ROCKET)
+        keys.add(new ItemBuilder(XMaterial.FIREWORK_ROCKET)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_FIREWORK))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_FIREWORK,
                         "%STATUS%", Main.getInstance().getPluginConfig().getShowFireworkAfterEggFound() ? "§a§l✔ Enabled" : "§c§l❌ Disabled"))
@@ -103,7 +104,7 @@ public class SettingsMenu extends InventoryMenu {
                 .withGlow(Main.getInstance().getPluginConfig().getShowFireworkAfterEggFound())
                 .build());
 
-        getInventory().setItem(20, new ItemBuilder(XMaterial.CLOCK)
+        keys.add(new ItemBuilder(XMaterial.CLOCK)
                 .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.SETTINGS_HINT_COOLDOWN))
                 .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.SETTINGS_HINT_COOLDOWN,
                         "%STATUS%", Main.getInstance().getPluginConfig().getHintApplyCooldownOnFail() ? "§a§l✔ Enabled" : "§c§l❌ Disabled"))
@@ -112,14 +113,58 @@ public class SettingsMenu extends InventoryMenu {
                 .build());
     }
 
-    @Override
-    public void handleMenu(InventoryClickEvent event) {
-        if (!ItemHelper.hasItemId(event.getCurrentItem())) {
+    public void addMenuBorderButtons() {
+        inventoryContent[49] = new ItemBuilder(XMaterial.BARRIER)
+                .setCustomId("settings.close")
+                .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.CLOSE_BUTTON))
+                .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.CLOSE_BUTTON))
+                .build();
+    }
+
+    public void setMenuItems() {
+        getInventory().setItem(48, new ItemBuilder(XMaterial.PLAYER_HEAD)
+                .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.PREVIOUS_PAGE_BUTTON, "%CURRENT_PAGE%", String.valueOf(page + 1), "%MAX_PAGES%", String.valueOf(getMaxPages())))
+                .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.PREVIOUS_PAGE_BUTTON))
+                .setSkullOwner(Main.getTexture("ZDU5YmUxNTU3MjAxYzdmZjFhMGIzNjk2ZDE5ZWFiNDEwNDg4MGQ2YTljZGI0ZDVmYTIxYjZkYWE5ZGIyZDEifX19"))
+                .setCustomId("settings.previous_page")
+                .build());
+        getInventory().setItem(50, new ItemBuilder(XMaterial.PLAYER_HEAD)
+                .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.NEXT_PAGE_BUTTON, "%CURRENT_PAGE%", String.valueOf(page + 1), "%MAX_PAGES%", String.valueOf(getMaxPages())))
+                .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.NEXT_PAGE_BUTTON))
+                .setSkullOwner(Main.getTexture("NDJiMGMwN2ZhMGU4OTIzN2Q2NzllMTMxMTZiNWFhNzVhZWJiMzRlOWM5NjhjNmJhZGIyNTFlMTI3YmRkNWIxIn19fQ=="))
+                .setCustomId("settings.next_page")
+                .build());
+
+        if (keys.isEmpty()) {
+            getInventory().setItem(22, new ItemBuilder(XMaterial.RED_STAINED_GLASS)
+                    .setDisplayName(menuMessageManager.getMenuItemName(MenuMessageKey.LIST_ERROR))
+                    .setLore(menuMessageManager.getMenuItemLore(MenuMessageKey.LIST_ERROR))
+                    .build());
             return;
         }
+        for(int i = 0; i < getMaxItemsPerPage(); i++) {
+            index = getMaxItemsPerPage() * page + i;
+            if (index >= keys.size()) break;
+            if (keys.get(index) == null) {
+                continue;
+            }
+            int slotIndex = ((9 + 1) + ((i / 7) * 9) + (i % 7));
+            getInventory().setItem(slotIndex,keys.get(index));
+        }
+    }
 
-        Player player = (Player) event.getWhoClicked();
+    public int getMaxItemsPerPage() {
+        return maxItemsPerPage;
+    }
+    public int getMaxPages(){
+        if(keys.isEmpty()) return 1;
+        return (int) Math.ceil((double) keys.size() / getMaxItemsPerPage());
+    }
+
+    @Override
+    public void handleMenu(InventoryClickEvent event) {
         SoundManager soundManager = Main.getInstance().getSoundManager();
+        Player player = (Player) event.getWhoClicked();
 
         switch (ItemHelper.getItemId(event.getCurrentItem())) {
             case "settings.close":
@@ -129,7 +174,7 @@ public class SettingsMenu extends InventoryMenu {
             case "settings.updater":
                 Main.getInstance().getPluginConfig().setUpdater(!Main.getInstance().getPluginConfig().getUpdater());
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.commandfeedback":
@@ -152,7 +197,7 @@ public class SettingsMenu extends InventoryMenu {
                     Main.getInstance().getPluginConfig().setSoundVolume(currentVolume - 1);
                 }
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.armorstandglow":
@@ -172,13 +217,13 @@ public class SettingsMenu extends InventoryMenu {
                     Main.getInstance().getPluginConfig().setArmorstandGlow(currentTime - 1);
                 }
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.showcoordinates":
                 Main.getInstance().getPluginConfig().setShowCoordinatesWhenEggFoundInProgressInventory(!Main.getInstance().getPluginConfig().getShowCoordinatesWhenEggFoundInProgressInventory());
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.eggnearbyradius":
@@ -198,27 +243,55 @@ public class SettingsMenu extends InventoryMenu {
                     Main.getInstance().getPluginConfig().setShowEggsNearbyMessageRadius(currentRadius - 1);
                 }
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.pluginprefix":
                 Main.getInstance().getPluginConfig().setPluginPrefixEnabled(!Main.getInstance().getPluginConfig().getPluginPrefixEnabled());
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.firework":
                 Main.getInstance().getPluginConfig().setShowFireworkAfterEggFound(!Main.getInstance().getPluginConfig().getShowFireworkAfterEggFound());
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
                 break;
             case "settings.hintcooldown":
                 Main.getInstance().getPluginConfig().setHintApplyCooldownOnFails(!Main.getInstance().getPluginConfig().getHintApplyCooldownOnFail());
                 Main.getInstance().getPluginConfig().saveData();
-                menuContent();
+                this.open();
                 player.playSound(player.getLocation(), soundManager.playInventorySuccessSound(), soundManager.getSoundVolume(), 1);
+                break;
+            case "settings.previous_page":
+                if (page == 0) {
+                    player.sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.FIRST_PAGE));
+                    player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventoryFailedSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                } else {
+                    page = page - 1;
+                    this.open();
+                    player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                }
+                break;
+            case "settings.next_page":
+                if (!((index + 1) >= keys.size())) {
+                    page = page + 1;
+                    this.open();
+                    player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventorySuccessSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                } else {
+                    player.sendMessage(Main.getInstance().getMessageManager().getMessage(MessageKey.LAST_PAGE));
+                    player.playSound(player.getLocation(), Main.getInstance().getSoundManager().playInventoryFailedSound(), Main.getInstance().getSoundManager().getSoundVolume(), 1);
+                }
                 break;
         }
     }
+
+    @Override
+    public void onOpen(InventoryOpenEvent event) {
+        Main.getInstance().setLastOpenedInventory(getInventory(), playerMenuUtility.getOwner());
+        getInventory().setContents(inventoryContent);
+        setMenuItems();
+    }
 }
+
