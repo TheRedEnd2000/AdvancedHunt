3ff074e6401f711ea9607f5a5626b959d974f514
TheRedEnd2000 <fkircher2007@gmail.com>
2025-04-21 17:02:44 +0200

    tried to do texture but crashes the client

--- src/main/java/de/theredend2000/advancedhunt/protocollib/BlockChangingManager.java
+++ src/main/java/de/theredend2000/advancedhunt/protocollib/BlockChangingManager.java
@@ -6,6 +6,9 @@ import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.*;
 import com.comphenix.protocol.wrappers.BlockPosition;
 import com.comphenix.protocol.wrappers.WrappedBlockData;
+import com.comphenix.protocol.wrappers.nbt.NbtCompound;
+import com.comphenix.protocol.wrappers.nbt.NbtFactory;
+import com.comphenix.protocol.wrappers.nbt.NbtList;
 import de.theredend2000.advancedhunt.Main;
 import de.theredend2000.advancedhunt.util.ItemBuilder;
 import de.theredend2000.advancedhunt.util.ItemHelper;
@@ -35,6 +38,7 @@ import org.bukkit.inventory.meta.BlockDataMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
@@ -218,33 +222,6 @@ public class BlockChangingManager extends PacketAdapter implements Listener {
     }
 
     /**
-     * Creates a ghost block by copying all properties of the original block.
-     *
-     * @param location Location where the ghost block should appear
-     * @param player Player who should see the ghost block
-     * @param material Material of the block
-     * @param blockData The complete BlockData including rotation, orientation, etc.
-     */
-    public void sendBlockChangePacket(Location location, Player player, Material material, BlockData blockData) {
-        PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.BLOCK_CHANGE);
-
-        BlockPosition blockPosition = new BlockPosition(
-                location.getBlockX(), location.getBlockY(), location.getBlockZ());
-
-        packet.getBlockPositionModifier().write(0, blockPosition);
-        packet.getBlockData().write(0, WrappedBlockData.createData(blockData));
-
-        try {
-            protocolManager.sendServerPacket(player, packet);
-            registerGhostBlock(player.getUniqueId(), location, material, blockData);
-            logHelper.sendLogMessage("§aCreated Ghostblock " + material.name() + " with data: " + blockData.getAsString());
-        } catch (Exception e) {
-            logHelper.sendLogMessage("§cError while creating a Ghost Block!");
-            e.printStackTrace();
-        }
-    }
-
-    /**
      * Creates a ghost block by copying all properties from the placed block.
      * This method supports copying properties from ItemMeta if available.
      *
@@ -257,22 +234,7 @@ public class BlockChangingManager extends PacketAdapter implements Listener {
         Material material = block.getType();
         BlockData blockData = block.getBlockData();
 
-        /*
-            TODO texture not working
-         */
-
-        if (itemMeta instanceof SkullMeta && material == Material.PLAYER_HEAD) {
-            ItemStack item = new ItemStack(material);
-            item.setItemMeta(itemMeta);
-            String texture = Main.getTexture(ItemHelper.getSkullTexture(item));
-            logHelper.sendLogMessage("Added texture value to head: "+texture);
-
-            ItemStack head = new ItemBuilder(material).setSkullOwner(texture).build();
-            BlockDataMeta bdMeta = (BlockDataMeta) head.getItemMeta();
-            blockData = bdMeta.getBlockData(material);
-            logHelper.sendLogMessage("Using SkullMeta from ItemMeta");
-        }
-
+        // Process BlockDataMeta for any block that supports it
         if (itemMeta instanceof BlockDataMeta) {
             BlockDataMeta blockDataMeta = (BlockDataMeta) itemMeta;
             if (blockDataMeta.hasBlockData()) {
@@ -281,7 +243,108 @@ public class BlockChangingManager extends PacketAdapter implements Listener {
             }
         }
 
-        sendBlockChangePacket(location, player, material, blockData);
+        // First send the normal block packet to set up the block with its orientation
+        PacketContainer blockPacket = protocolManager.createPacket(PacketType.Play.Server.BLOCK_CHANGE);
+        BlockPosition blockPosition = new BlockPosition(
+                location.getBlockX(), location.getBlockY(), location.getBlockZ());
+        blockPacket.getBlockPositionModifier().write(0, blockPosition);
+        blockPacket.getBlockData().write(0, WrappedBlockData.createData(blockData));
+
+        try {
+            protocolManager.sendServerPacket(player, blockPacket);
+
+            // Handle player head textures separately with a tile entity packet
+            if (itemMeta instanceof SkullMeta && material == Material.PLAYER_HEAD) {
+                ItemStack item = new ItemStack(material);
+                item.setItemMeta(itemMeta);
+                String texture = Main.getTexture(ItemHelper.getSkullTexture(item));
+                logHelper.sendLogMessage("Added texture value to head: " + texture);
+
+                // Create and send the tile entity packet to set the texture
+                PacketContainer tileEntityPacket = protocolManager.createPacket(PacketType.Play.Server.TILE_ENTITY_DATA);
+
+                // Set the block position
+                tileEntityPacket.getBlockPositionModifier().write(0, blockPosition);
+
+                // Check if the packet uses TileEntityType enum in newer versions
+                try {
+                    // For newer versions - use reflection to get the proper enum
+                    Class<?> tileEntityTypeClass = Class.forName("net.minecraft.world.level.block.entity.TileEntityTypes");
+                    // The actual class path might vary by MC version - adjust if needed
+
+                    Object skullType = null;
+                    for (Field field : tileEntityTypeClass.getDeclaredFields()) {
+                        if (field.getName().equals("SKULL") || field.getName().equals("HEAD")) {
+                            field.setAccessible(true);
+                            skullType = field.get(null);
+                            break;
+                        }
+                    }
+
+                    if (skullType != null) {
+                        tileEntityPacket.getModifier().write(1, skullType);
+                    } else {
+                        // Fallback - try to use the action integer but check array length first
+                        if (tileEntityPacket.getIntegers().size() > 0) {
+                            tileEntityPacket.getIntegers().write(0, 5); // Updated skull type value
+                        } else {
+                            logHelper.sendLogMessage("§cWarning: Could not set skull type in packet");
+                        }
+                    }
+                } catch (Exception e) {
+                    // Alternative approach - get the correct indexes by inspecting the packet
+                    logHelper.sendLogMessage("§cError setting tile entity type: " + e.getMessage());
+
+                    // Debug the packet structure to find correct fields
+                    logHelper.sendLogMessage("§ePacket structure: " + tileEntityPacket.getStructures());
+
+                    // Try using a different modifier if integers array is empty
+                    try {
+                        // Some versions use byte for the action type instead of integer
+                        tileEntityPacket.getBytes().write(0, (byte)5);
+                    } catch (Exception e2) {
+                        logHelper.sendLogMessage("§cCould not set tile entity type: " + e2.getMessage());
+                    }
+                }
+
+                // Create NBT data for the skull texture
+                NbtCompound nbt = NbtFactory.ofCompound("");
+                nbt.put("id", "minecraft:skull"); // Add the id field which might be required
+
+                // Set the skull type to player
+                nbt.put("SkullType", (byte) 3);
+
+                NbtCompound skullOwner = NbtFactory.ofCompound("SkullOwner");
+                String uuidString = UUID.randomUUID().toString();
+                skullOwner.put("Id", uuidString);
+
+                // Create texture properties
+                NbtCompound properties = NbtFactory.ofCompound("Properties");
+                NbtList textures = NbtFactory.ofList("textures");
+                NbtCompound textureValue = NbtFactory.ofCompound("");
+                textureValue.put("Value", texture);
+                textures.add(textureValue);
+                properties.put("textures", textures);
+                skullOwner.put("Properties", properties);
+
+                // Add SkullOwner to main compound
+                nbt.put("SkullOwner", skullOwner);
+
+                // Write NBT data to packet
+                tileEntityPacket.getNbtModifier().write(0, nbt);
+
+                // Send the tile entity packet
+                protocolManager.sendServerPacket(player, tileEntityPacket);
+                logHelper.sendLogMessage("§aApplied skull texture to ghost block");
+            }
+
+            // Register the ghost block with its blockdata
+            registerGhostBlock(player.getUniqueId(), location, material, blockData);
+            logHelper.sendLogMessage("§aCreated Ghostblock " + material.name() + " with data: " + blockData.getAsString());
+        } catch (Exception e) {
+            logHelper.sendLogMessage("§cError while creating a Ghost Block!");
+            e.printStackTrace();
+        }
     }
 
     // -------------------- EVENT LISTENERS --------------------
