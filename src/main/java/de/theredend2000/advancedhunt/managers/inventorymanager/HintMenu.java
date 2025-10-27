ef5553b9572a1a466026627fb5a6e17a7104e2c8
GrafterCrafter <fakeEmail@noreply.com>
2025-10-27 20:24:37 +0000

    Fix ConfigLocationUtil root/path

--- src/main/java/de/theredend2000/advancedhunt/managers/inventorymanager/HintMenu.java
+++ src/main/java/de/theredend2000/advancedhunt/managers/inventorymanager/HintMenu.java
@@ -134,7 +134,7 @@ public class HintMenu extends InventoryMenu {
             }
             if (Main.getInstance().getEggManager().checkFoundAll(player, collection)) continue;
             int number = Main.getInstance().getEggManager().getRandomNotFoundEgg(player, collection);
-            ConfigLocationUtil location = new ConfigLocationUtil(plugin, "PlacedEggs." + number + ".");
+            ConfigLocationUtil location = new ConfigLocationUtil(plugin, "PlacedEggs." + number);
             if (location.loadLocation(collection) != null) {
                 int random = Main.getInstance().getRandom().nextInt(2);
                 return Main.getInstance().getMessageManager().getMessage(MessageKey.EGG_HINT).replaceAll("%X%", random == 1 ? String.valueOf(location.loadLocation(collection).getBlockX()) : "§k1").replaceAll("%Y%", String.valueOf(location.loadLocation(collection).getBlockY())).replaceAll("%Z%", random == 0 ? String.valueOf(location.loadLocation(collection).getBlockZ()) : "§k1");
