b376e10c01dbe34ef13f694edd0cf85dffa37292
TheRedEnd2000 <fkircher2007@gmail.com>
2025-12-26 23:33:19 +0100

    changed static method

--- src/main/java/de/theredend2000/advancedhunt/Main.java
+++ src/main/java/de/theredend2000/advancedhunt/Main.java
@@ -7,6 +7,7 @@ import de.theredend2000.advancedhunt.data.YamlRepository;
 import de.theredend2000.advancedhunt.listeners.*;
 import de.theredend2000.advancedhunt.managers.*;
 import de.theredend2000.advancedhunt.managers.minigame.MinigameManager;
+import de.theredend2000.advancedhunt.menu.Button;
 import de.theredend2000.advancedhunt.placeholder.AdvancedHuntExpansion;
 import de.theredend2000.advancedhunt.util.updater.PluginUpdater;
 import org.bstats.bukkit.Metrics;
@@ -48,6 +49,7 @@ public final class Main extends JavaPlugin {
         // Initialize Message Manager
         messageManager = new MessageManager(this);
         soundManager = new SoundManager(this);
+        new Button(this); // register plugin for button
 
         // Initialize Data Repository
         String storageType = getConfig().getString("storage.type", "YAML");
